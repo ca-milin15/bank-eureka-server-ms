b5a2cea6d6fad09733e9e671c481fa4a8b8c5334
User Developer <backenddeveloper30@gmail.com>
2024-06-06 11:43:25 -0500

    Configuracion basica de Eureka server

--- src/main/java/com/pichincha/bank_eureka_server_ms/BankEurekaServerMsApplication.java
+++ src/main/java/com/pichincha/bank_eureka_server_ms/BankEurekaServerMsApplication.java
@@ -2,7 +2,9 @@ package com.pichincha.bank_eureka_server_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class BankEurekaServerMsApplication {
 
