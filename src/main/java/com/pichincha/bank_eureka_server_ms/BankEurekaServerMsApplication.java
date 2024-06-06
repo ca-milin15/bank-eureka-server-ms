5666daebfb785faf148364e073c64d956bfe8102
User Developer <backenddeveloper30@gmail.com>
2024-06-05 22:33:14 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/pichincha/bank_eureka_server_ms/BankEurekaServerMsApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_eureka_server_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEurekaServerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEurekaServerMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/bank_eureka_server_ms/BankEurekaServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_eureka_server_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEurekaServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
